3462b50c41f9c7930a2c5c040c462a1f93c16aeb
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-03-17 23:04:08 +0100

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+const botonHamburguesa = document.getElementById("hamburguesa");
+
+
+function iniciar(){
+
+
+    botonHamburguesa.addEventListener("click",(event)=>{
+        console.log(event);
+    })
+
+}
