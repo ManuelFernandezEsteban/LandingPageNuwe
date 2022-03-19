8014d53a7021f25562d20847c5e78bd44cc7754b
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-03-19 13:54:43 +0100

    menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 const botonHamburguesa = document.getElementById("hamburguesa");
+const divMenuMovil = document.getElementById("menuMovil");
 
 
 function iniciar(){
@@ -7,6 +8,16 @@ function iniciar(){
 
     botonHamburguesa.addEventListener("click",(event)=>{
         console.log(event);
+        if (divMenuMovil.classList.contains('visible')){
+            divMenuMovil.classList.remove('visible');
+            botonHamburguesa.style.transform='rotate(-90deg)';
+        }
+        else{
+            divMenuMovil.classList.add('visible');
+            botonHamburguesa.style.transform='rotate(180deg)';
+            
+        }
     })
+
 
 }
