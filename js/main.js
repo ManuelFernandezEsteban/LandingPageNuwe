e26b7c83536f839208e8930de79a0ff7ded88cda
Manuel1977 <Manuel_fe1977@hotmail.com>
2022-03-20 12:29:57 +0100

    vista movil terminada

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
 
 const botonHamburguesa = document.getElementById("hamburguesa");
 const divMenuMovil = document.getElementById("menuMovil");
+const liMenu = document.querySelectorAll(".menu > li");
 
 
-function iniciar(){
+
+function iniciar() {
+
+    liMenu.forEach(li => {
+        li.addEventListener("click", (event) => {
+            setTimeout(() => {
+                const subMenu = li.nextSibling.nextSibling;
+                const flecha = li.children[1];
+                if (subMenu.classList.contains('visible')) {
+                    subMenu.classList.remove('visible');                    
+                    flecha.style.transform='rotate(180deg)'
+                    
+                } else {
+                    subMenu.classList.add('visible');
+                    flecha.style.transform='rotate(360deg)';                    
+                }
+            }, 150);
+
+        })
+    });
+
+    botonHamburguesa.addEventListener("click", (event) => {
+
+        setTimeout(() => {
+            if (divMenuMovil.classList.contains('visible')) {
+                divMenuMovil.classList.remove('visible');
+                botonHamburguesa.style.transform = 'rotate(-90deg)';
+            }
+            else {
+                divMenuMovil.classList.add('visible');
+                botonHamburguesa.style.transform = 'rotate(180deg)';               
+
+            }
+        }, 150);
 
 
-    botonHamburguesa.addEventListener("click",(event)=>{
-        console.log(event);
-        if (divMenuMovil.classList.contains('visible')){
-            divMenuMovil.classList.remove('visible');
-            botonHamburguesa.style.transform='rotate(-90deg)';
-        }
-        else{
-            divMenuMovil.classList.add('visible');
-            botonHamburguesa.style.transform='rotate(180deg)';
-            
-        }
     })
 
 
